edfef16cef38ea8833c6aa0fa2aa6943bae8ac0d
Adam Aizal <100889799+adamazl@users.noreply.github.com>
2023-10-02 16:33:34 +1300

    added basic express server

--- backend.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, jsonify
-
-app = Flask(__name__)
-
-@app.route('/api/recipes', methods=['GET'])
-def get_recipes():
-    # Fetch recipes from database
-    return jsonify({"recipes": []})
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// TODO: Add routes here
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
