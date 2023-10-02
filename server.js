19caf74790b9ea59c397e084df54e02ba70550e0
Adam Aizal <100889799+adamazl@users.noreply.github.com>
2023-10-02 16:54:15 +1300

    added mysql connection

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ const cors = require('cors');
 const app = express();
 const PORT = 3000;
 
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'database-1.crebsosieeok.us-east-1.rds.amazonaws.com',
+    user: 'admin',
+    password: 'Quack1nce4^',
+    database: 'recipes_db'
+});
+
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
